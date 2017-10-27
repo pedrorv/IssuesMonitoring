77ac508389bbadc6d17327e2d639ce9a18132ad9
Pedro Reis <pedroreisv@gmail.com>
2017-10-27 16:13:25 -0200

    Refactor API to authenticator.py

--- server/issues_monitoring/views/__init__.py
+++ server/issues_monitoring/views/__init__.py
@@ -1,3 +1,4 @@
 from .arduino import *
 from .web import *
 from .errors import *
+from .authenticator import *
--- server/issues_monitoring/views/web.py
+++ server/issues_monitoring/views/web.py
@@ -916,17 +916,4 @@ def relatorio_pdf(nome):
                     headers=header)
 
 
-@app.route('/validar_usuario_authenticator', methods=["POST"])
-def validar_usuario_authenticator():
-    conteudo = request.get_json(silent=True)
-    login = conteudo.get('login')
-    senha = conteudo.get('senha')
-    validacao = controllers.validar_usuario_authenticator(login, senha)
-    resposta = jsonify(validacao)
-
-    if 'erro' in validacao:
-        resposta.status_code = 401
-        return resposta
-    
-    return resposta
 
